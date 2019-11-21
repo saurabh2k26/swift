d6b2bdf525fddd70845f1fae7019c8c95f374f2a
saurabh2k26 <saurabh2k26@gmail.com>
2019-11-21 14:47:18 +0530

    Create array.swift

---
+++ code/array.swift
@@ -0,0 +1,13 @@
+var arr = ["rohit" , "virat", "shikhar"]
+print(arr)  //printing array
+//accessing elements
+for i in arr{
+    print(i)
+}
+print(arr[0])  //first element
+print(arr.last!)  //last element
+print(arr.count)  // length of array
+
+var my_array = [String]()  // creating a new blank array with element type - string
+my_array.append("Sachin")
+my_array += ["Ganguly", "Dravid"]
