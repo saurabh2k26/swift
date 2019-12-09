465a26324aa79e3a33664e1e6c941982cc939db4
saurabh2k26 <saurabh2k26@gmail.com>
2019-12-09 16:05:32 +0530

    Create strings.swift

---
+++ code/strings.swift
@@ -0,0 +1,6 @@
+var mystr = "Saurabh"
+print(mystr.count)  // length of string
+print(mystr.hasPrefix("Sa"))
+print(mystr.hasSuffix("Sa"))
+print(mystr.prefix(4))
+print(String(Array(mystr.characters)[2...5]))
