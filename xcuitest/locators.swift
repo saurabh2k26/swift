3e6ec34d33ae63298e8704b54318740083848649
saurabh2k26 <saurabh2k26@gmail.com>
2019-12-10 15:04:26 +0530

    Update locators.swift

--- xcuitest/locators.swift
+++ xcuitest/locators.swift
@@ -4,3 +4,10 @@ let allButtons = app.descendants(matching: .button)
         for i in 0...allButtons.count-1 {
             print(allButtons.element(boundBy: i).label)
         }
+
+// find all buttons - direct
+let app = XCUIApplication()
+let allButtons = app.buttons
+        for i in 0...allButtons.count-1 {
+            print(allButtons.element(boundBy: i).label)
+        }
